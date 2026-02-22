beb9cc905a591544bda752da5dfddb5ca0e3a09c
GeniusMachado <geniusmachado@gmail.com>
2026-02-22 15:28:42 -0800

    included the dependencies in pyproject.toml file for sqlalchemy and pymysql. Also got rid of unwanted Student class that was declared during the begining of the project

--- schemas.py
+++ schemas.py
@@ -24,13 +24,3 @@ class FileUpload(BaseModel):
     status: str
     reason: str
     uploaded_at: Optional[datetime] = None
-
-
-class Student:
-    def __init__(self, name: str, age: int, student_id: str):
-        self.name = name
-        self.age = age
-        self.student_id = student_id
-
-    def __repr__(self):
-        return f"Student(name={self.name}, age={self.age}, student_id={self.student_id})"
